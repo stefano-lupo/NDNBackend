802900e0d64d9dfc5fb0111d386d32a6b828a0c5
Stefano <lupos@tcd.ie>
2018-11-26 20:42:40 +0000

    Ping Pong working

---
+++ src/main/java/com/stefanolupo/ndntesting/Pinger.java
@@ -0,0 +1,88 @@
+package com.stefanolupo.ndntesting;
+
+import net.named_data.jndn.*;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Pinger implements OnData, OnTimeout {
+
+  private AtomicBoolean outstandingRequest = new AtomicBoolean(false);
+  private AtomicInteger nextPing = new AtomicInteger(0);
+
+  private final String pingLocation;
+  private final Face face;
+  private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+
+  Pinger(String pingLocation) {
+    this.pingLocation = pingLocation;
+    this.face = new Face();
+    scheduler.scheduleAtFixedRate(this::checkForEvents, 0, 1, TimeUnit.SECONDS);
+  }
+
+  @Override
+  public void onData(Interest interest, Data data) {
+    String name = data.getName().toUri();
+    System.out.println("DATA: " + name + ": " + data.getContent().toString() + "\n");
+    nextPing.incrementAndGet();
+    outstandingRequest.set(false);
+  }
+
+  @Override
+  public void onTimeout(Interest interest) {
+    System.out.println("Timeout for interest: " + interest.getName().toUri() + "\n");
+    nextPing.incrementAndGet();
+    outstandingRequest.set(false);
+  }
+
+  private void checkForEvents() {
+    try {
+      face.processEvents();
+    } catch (Exception e) {
+      System.err.println("Failed to process events");
+      e.printStackTrace();
+    }
+  }
+
+  private Name getNextPing() {
+    return new Name(pingLocation + "/" + nextPing.get());
+  }
+
+  public void endlessPing(long sleepMs) {
+    while (true) {
+      if (!outstandingRequest.get()) {
+        ping();
+      } else {
+        try {
+          Thread.sleep(sleepMs);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+
+    }
+  }
+
+  public void ping() {
+    try {
+      Name nextPing = getNextPing();
+      System.out.println("Expressing interest for: " + nextPing.toUri());
+      face.expressInterest(nextPing, this, this);
+      face.processEvents();
+      outstandingRequest.set(true);
+    } catch (Exception e) {
+      System.err.println("Could not express interest");
+      e.printStackTrace();
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("Starting pinger!");
+    Pinger pinger = new Pinger("ndn:/com/stefanolupo/lupos/ping");
+    pinger.endlessPing(5000);
+  }
+
+}
---
+++ src/main/java/com/stefanolupo/ndntesting/Ponger.java
@@ -0,0 +1,55 @@
+package com.stefanolupo.ndntesting;
+
+import net.named_data.jndn.*;
+import net.named_data.jndn.security.KeyChain;
+import net.named_data.jndn.transport.Transport;
+import net.named_data.jndn.util.Blob;
+
+
+public class Ponger implements OnInterest {
+
+  private int version = 1;
+  private final KeyChain keyChain;
+  private final Face face;
+  private final Name certificateName;
+
+  public static void main(String[] args) throws Exception {
+
+    Ponger ponger = new Ponger();
+    ponger.processEvents();
+  }
+
+  public Ponger() throws Exception {
+    keyChain = new KeyChain();
+    certificateName =  keyChain.getIdentityManager().getDefaultCertificateNameForIdentity(new Name("/com/stefanolupo/lupos"));
+
+    face = new Face();
+    face.setCommandSigningInfo(keyChain, certificateName);
+
+    Name prefix = new Name("ndn:/com/stefanolupo/lupos");
+    face.registerPrefix(prefix, this,  failedPrefix -> System.out.println("Failed to register prefix " + failedPrefix.toUri()));
+  }
+
+  public void processEvents() throws Exception {
+    while (true) {
+      face.processEvents();
+      Thread.sleep(100);
+    }
+  }
+
+  @Override
+  public void onInterest(Name prefix, Interest interest, Transport transport, long registeredPrefixId)  {
+    System.out.println("Got interest for: " + prefix.toUri() + " - " + interest.getName().toUri());
+
+    Name dataName = new Name(interest.getName());
+    Data data = new Data(dataName).setContent(new Blob("derp"));
+
+    try {
+      keyChain.sign(data, certificateName);
+      transport.send(data.getDefaultWireEncoding().buf());
+      System.out.println("Sent: " + data.getName().toUri() + "\n");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+}
