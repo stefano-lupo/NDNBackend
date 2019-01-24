e34ef2f11b0d4f018183d286ace4e9c908fdc682
Stefano Lupo <lupos@tcd.ie>
2019-01-24 12:24:01 +0000

    Fix proto script

--- compile_protos.sh
+++ compile_protos.sh
@@ -1,1 +1,4 @@
-protoc NDNCore/src/main/resources/NDNGame.proto --java_out NDNCore/target/generated-sources/
+#! /bin/bash
+out_dir="./NDNCore/target/generated-sources"
+mkdir -p $out_dir
+protoc NDNCore/src/main/resources/NDNGame.proto --java_out $out_dir
