89417b146bf4358be41f2705f700643de0bb2713
Stefano Lupo <lupos@tcd.ie>
2019-02-05 17:48:38 +0000

    Add actual LibGdxCode

---
+++ NdnGameLibGdx/desktop/src/com/stefnaolupo/ndngame/libgdx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.stefnaolupo.ndngame.libgdx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.stefnaolupo.ndngame.libgdx.LibGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LibGdxGame(), config);
+	}
+}
